da642cf538d2e1a445be9385ca0b54b904664b6a
Oluwatemmy <69505350+Oluwatemmy@users.noreply.github.com>
2024-06-20 09:03:11 +0200

    Added OTP to verify email

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from .serilaizers import UserRegisterSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework import status
 from .utils import send_code_to_user
+from .models import OneTimePassword
 
 
 # Create your views here.
@@ -21,8 +22,37 @@ class RegisterUserView(GenericAPIView):
             # send email function to user['email']
             send_code_to_user(user["email"])
 
-            return Response({
-                "data": user,
-                "message": f"Hi, {user['first_name']}. Thanks for signing up a passcode has been sent "
-            }, status=status.HTTP_201_CREATED)
+            return Response(
+                {
+                    "data": user,
+                    "message": f"Hi, {user['first_name']}. Thanks for signing up a passcode has been sent ",
+                },
+                status=status.HTTP_201_CREATED,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VerifyUserEmail(GenericAPIView):
+
+    def post(self, request):
+        otpcode = request.data.get("otp")
+
+        try:
+            user_code_obj = OneTimePassword.objects.get(code=otpcode)
+            user = user_code_obj.user
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+                return Response(
+                    {"message": "Account email verified successfully"},
+                    status=status.HTTP_200_OK,
+                )
+            return Response(
+                {"message": "User is already verified"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        except OneTimePassword.DoesNotExist:
+            return Response(
+                {"message": "Passcode does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
