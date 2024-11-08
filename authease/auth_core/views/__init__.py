976d7dd73da12f1d1db5d00536b3db6f3e9729c3
Oluwatemmy <oluwaseyitemitope456@gmail.com>
2024-11-08 20:20:40 +0100

    Add CI/CD workflow for PyPI release

--- authease/auth_core/views/__init__.py
+++
@@ -1,2 +0,0 @@
-from .authentication_views import LoginUserView, TestAuthenticationView, RegisterUserView, VerifyUserEmail, LogoutView
-from .password_views import PasswordResetRequestView, PasswordResetConfirm, SetNewPassword
--- authease/build/lib/oauth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import GoogleSignInView, GithubSignInView
-
-
-urlpatterns = [
-    path('google/', GoogleSignInView.as_view(), name='google-sign-in'),
-    path('github/', GithubSignInView.as_view(), name='github-sign-in'),
-]
