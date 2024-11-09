3950cc75181f39eea339d7ab5e4266b7c96c1664
Oluwatemmy <oluwaseyitemitope456@gmail.com>
2024-11-09 14:50:36 +0100

    fixed auth_user_model issue

--- authease/auth_core/apps.py
+++ authease/auth_core/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class AuthCoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth_core'
+    name = 'auth_ease.auth_core'
+    label = 'auth_core'
